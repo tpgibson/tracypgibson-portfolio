d2f521a519285a80087e59f887552bbcf665fdec
Tracy Gibson <tracygibson2002@gmail.com>
2016-08-23 12:39:58 -0400

    added validation

---
+++ scripts/keys.js
@@ -0,0 +1,8 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyB42Op5AuVY0-iROQkP2JbdcUoxyQs7z98",
+    authDomain: "tracypgibson-portfoliosite.firebaseapp.com",
+    databaseURL: "https://tracypgibson-portfoliosite.firebaseio.com",
+    storageBucket: "tracypgibson-portfoliosite.appspot.com",
+};
+firebase.initializeApp(config);
