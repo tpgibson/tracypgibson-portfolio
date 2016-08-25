aed3225295441eb7360f85cc503841f691bfeb6b
Tracy Gibson <tracygibson2002@gmail.com>
2016-08-25 12:41:08 -0400

    homepage mobile and tablet/desktop finalized

--- README.md
+++
@@ -1,2 +0,0 @@
-# tracypgibson-portfolio
-My portfolio website.
--- scripts/keys.js
+++
@@ -1,8 +0,0 @@
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyB42Op5AuVY0-iROQkP2JbdcUoxyQs7z98",
-    authDomain: "tracypgibson-portfoliosite.firebaseapp.com",
-    databaseURL: "https://tracypgibson-portfoliosite.firebaseio.com",
-    storageBucket: "tracypgibson-portfoliosite.appspot.com",
-};
-firebase.initializeApp(config);
